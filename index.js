a8189b26432912c2b931675db18e43e680cb527d
Collins Wekesa <collins.wekesa@andela.com>
2017-04-05 20:08:03 +0300

    add files

---
+++ index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM, { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router, browserHistory } from 'react-router';
+import configureStore from './client/store/ConfigureStore';
+import { loadUsers } from './client/actions/UsersAction';
+import routes from './client/config/routes';
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
+
+const store = configureStore();
+store.dispatch(loadUsers());
+
+render(
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>,
+  document.getElementById('app')
+);
